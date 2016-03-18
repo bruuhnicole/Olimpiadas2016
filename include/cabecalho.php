ab26c1e4628c940d5a638000e60d90945237e471
Fabiano Rodrigues <fabiano_rt@hotmail.com>
2016-03-17 22:08:03 -0300

    Correção de variável do cabeçalho

--- include/cabecalho.php
+++ include/cabecalho.php
@@ -2,12 +2,16 @@
 <html>
 <head>
 	<meta charset="UTF-8" />
-	<title><?php print $titulo; ?></title>
+	<title><?php print @$cabecalho_title; ?></title>
 	<link rel="stylesheet" href="http://localhost:8080/style/estilos.css">
 	<?php print @$style; ?>
 	<meta name="viewport" content="width=device-width">
 </head>
 <body>
 	<header>
-
+		<ul>
+			<li><a href="#">Modalidades</a></li>
+			<li><a href="#">Calendário</a></li>
+			<li><a href="#">Belo Horizonte</a></li>
+		</ul>
 	</header>
