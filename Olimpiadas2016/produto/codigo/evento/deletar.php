f5d871da9577e960f26a65e6fd0720a3c137a527
Felipe Milagres <felipe.milagres07@gmail.com>
2016-05-03 00:36:23 -0300

    Listar e deletar - Evento

---
+++ Olimpiadas2016/produto/codigo/evento/deletar.php
@@ -0,0 +1,18 @@
+﻿<?php
+require_once("../include/conexaoBD.php");
+
+    //captura os dados passados como GET
+    $codigoEvento = utf8_encode(htmlspecialchars($_GET['codEvento']));
+        
+    $sql = "DELETE FROM evento WHERE codEvento=?";
+        
+    $stmt = $conn->prepare($sql); 
+        
+    $stmt->bind_param('i', $codigoEvento);
+
+    $stmt->execute(); 
+    $stmt->close();
+    $conn->close();           
+               
+    header("Location: ./listar.php#eventos");
+?>
--- Olimpiadas2016/produto/codigo/evento/listar.php
+++ Olimpiadas2016/produto/codigo/evento/listar.php
@@ -1,46 +1,53 @@
 ﻿<?php
 include("../include/cabecalho.php"); 
 include("../include/administrador.php");
+require_once("../include/conexaoBD.php");
+
+$msg = "";
+
+$msg = isset($_GET['msg']) ? $_GET['msg']: "";
+$sql = "select nome, descricao, codEvento, DATE_FORMAT(dataInicio, '%d/%m/%Y') as data, TIME_FORMAT(dataInicio, '%H:%i') as horario from evento natural join modalidade";
+$result = $conn->query($sql); 
 ?>
+
     <!--Listar-->
-    <div class="section scrollspy" id="eventos">
+    <div name="form-anchor" class="section scrollspy" id="eventos">
         <div class="container">
             <div class="row">
                 <h2><b>Eventos</b></h2>
                 <div class="row">
-                    <a href="./cadastrar.html#cadastro" class="btn btn-info"><span class="glyphicon glyphicon-plus"></span> Cadastrar Evento</a>
+                    <a href="./cadastrar.php#cadastro" class="btn btn-info"><span class="glyphicon glyphicon-plus"></span> Cadastrar Evento</a>
                 </div>
                 <br />
                 <div class="row">
                     <fieldset>
                         <div class="table-responsive">
                             <table class="table table-bordered table-hover table-striped">
-                                <thead>
-                                    <tr>
-                                        <td><b>Evento</b></td>
-                                        <td><b>Modalidade</b></td>
-                                        <td><b>Data</b></td>
-                                        <td><b>Hora</b></td>
-                                        <td><b>Editar</b></td>
-                                        <td><b>Excluir</b></td>
-                                    </tr>
-                                </thead>
-                                <tr>
-                                    <td>Fases de grupo masculina e/ou feminina (4 jogos)</td>
-                                    <td>Vôlei de Praia</td>
-                                    <td>07/08/2016</td>
-                                    <td>10:00</td>
-                                    <td><a href="./editar.html#edicao" class="btn btn-success"><span class="glyphicon glyphicon-edit"></span></a></td>
-                                    <td><a href="#" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span></a></td>
-                                </tr>
-                                <tr>
-                                    <td>Primeira fase feminino (2 jogos)</td>
-                                    <td>Futebol</td>
-                                    <td>20/08/2016</td>
-                                    <td>15:00</td>
-                                    <td><a href="./editar.html#edicao" class="btn btn-success"><span class="glyphicon glyphicon-edit"></span></a></td>
-                                    <td><a href="#" class="btn btn-danger"><span class="glyphicon glyphicon-trash"></span></a></td>
-                                </tr>
+                                <?php
+                                if ($result->num_rows> 0) {                                   
+                                
+                                    echo "<thead><tr>";                                    
+                                    echo  "<td><b>Evento</b></td>";
+                                    echo  "<td><b>Modalidade</b></td>";
+                                    echo  "<td><b>Data</b></td>";
+                                    echo  "<td><b>Hora</b></td>";
+                                    echo  "<td><b>Editar</b></td>";
+                                    echo  "<td><b>Excluir</b></td>"; 
+                                    echo "</tr></thead>";                                    
+
+                                    while($row=$result->fetch_assoc())
+                                    {                                         
+                                        echo "<tr>";
+                                        echo "<td>".$row['nome']."</td>";
+                                        echo "<td>".$row['descricao']."</td>";
+                                        echo "<td>".$row['data']."</td>"; 
+                                        echo "<td>".$row['horario']."</td>"; 
+                                        echo "<td><a href='./editar.php#editar' class='btn btn-success'><span class='glyphicon glyphicon-edit' ></td>";  
+                                        echo "<td><a href='./deletar.php?codEvento=".htmlspecialchars($row['codEvento'])."' class='btn btn-danger'><span  class='glyphicon glyphicon-trash'></span></a></td>";                        
+                                        echo "<tr>";
+                                    }
+                                }
+                                ?>
                             </table>
                         </div>
                     </fieldset>
